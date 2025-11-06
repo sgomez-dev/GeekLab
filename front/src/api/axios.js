4fcee351a98137a13d8a267dbff4556bcb3b54f2
Santiago Gómez de la Torre Romero <gomezdelatorre15@gmail.com>
2025-11-07 00:05:27 +0100

    Update package-lock.json to remove peer dependencies and change axios base URL to local server address.

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'http://15.15.15.7:32131/api', // This is my local server, use localhost:4000
 });
 
 api.interceptors.request.use((config) => {
